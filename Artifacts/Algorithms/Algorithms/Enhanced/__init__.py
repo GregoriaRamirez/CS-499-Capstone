03b7d23d5817ec0220d0dd43dbdd240e14beeff7
GregoriaRamirez <141765686+GregoriaRamirez@users.noreply.github.com>
2025-08-12 02:16:27 -0400

    Delete Artifacts/Algorithms/Algorithms/Enhanced directory

--- Artifacts/Algorithms/Algorithms/Enhanced/__init__.py
+++
@@ -1,7 +0,0 @@
-# written by : Gregoria Ramirez
-# simple package setup for imort get_data from model.py for easy use
-
-
-# This file makes this folder a Python package
-from .model import get_data
-
